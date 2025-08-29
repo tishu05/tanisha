bbf1de4b3c227a045c4421d6def6d8c39d7d9dbb
tishu05 <tishuc542007@gmail.com>
2025-08-29 13:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
+import as os
 
 
 # Load data
@@ -43,5 +44,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port, debug=False)
 
