b164a462ed98b935c05cfb9c7314c9013e482448
tishu05 <tishuc542007@gmail.com>
2025-08-29 13:28:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
-import os
 
 
 # Load data
@@ -44,8 +43,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5000)
-    app.run(host='0.0.0.0',port=port, debug=False)
-
-
+    app.run(debug=True)
 
